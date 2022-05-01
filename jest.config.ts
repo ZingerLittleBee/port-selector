83e7865677bad85d953d9e31e9f7e8192bea14a5
ZingerLittleBee <6970999@gmail.com>
2022-05-02 00:19:06 +0800

    init: 🎉 jest ts-jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-node'
+}
